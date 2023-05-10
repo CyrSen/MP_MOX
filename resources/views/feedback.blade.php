170ac3c8cd8d06c65f40e4b950741c570b2114a0
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-05-10 14:16:33 +0200

    added custom iconsets smiley, testing them

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -6,36 +6,28 @@
 
 
 <div class="container-fluid">
-    <div class="container card ">
-        <div class="row justify-content-center px-5 pt-5">
+    <div class="container card">
+        <div class="row justify-content-center px-5 pt-3">
             <div class="card col-sm-12 m-2 p-3">
     
                 <h4 class="my-3">An diese Stelle hast du die Möglickkeit für deinen Arbeitsplatz ein Feedback abzugeben:</h4>
             </div>
         </div>
     
-        <div class="row justify-content-center mb-5">
-            <div class="card col-sm-8 m-2 p-3 order-last order-sm-first">
+        <div class="row justify-content-center ">
+            <div class="col-sm-8">
                 <div class="container-fluid">
                     <div class="row my-2">
                         <div class="col-sm-12 mb-2">
-                            <div class="card text-start">
-                                <i class="fa-solid fa-1 p-3"></i>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 mb-2">
-                            <div class="card text-start">
-                                <i class="fa-solid fa-2 p-3"></i>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 mb-2">
-                            <div class="card text-start">
-                                <i class="fa-solid fa-3 p-3"></i>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 mb-2">
-                            <div class="card text-start">
-                                <i class="fa-solid fa-4 p-3"></i>
+                            <div class="card">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-sm-1 my-3">
+                                <img class="" src="./assets/img/numbers_in_circle/number_one_circle.svg" alt="number_one">
+                                {{-- <i class="fa-solid fa-1 p-3"></i> --}}
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-sm-12 mb-2">
@@ -43,32 +35,80 @@
                                 <div class="container">
                                     <div class="row">
                                         <div class="col-sm-1 my-3">
-                                            <i class="fa-solid fa-5 fa-2xl my-3 p-3"></i>
+                                <img class="" src="./assets/img/numbers_in_circle/number_two_circle.svg" alt="number_two">
+                                {{-- <i class="fa-solid fa-2 p-3"></i> --}}
                                         </div>
-                                        <div class="col-sm-3 my-3 text-start">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 mb-2">
+                            <div class="card">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-sm-1 my-3">
+                                <img class="" src="./assets/img/numbers_in_circle/number_three_circle.svg" alt="number_three">
+                                {{-- <i class="fa-solid fa-3 p-3"></i> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 mb-2">
+                            <div class="card">
+                                <div class="container">
+                                    <div class="row my-0 p-2">
+                                        <div class="col-sm-2 p-2">
+                                            <img class="" src="./assets/img/numbers_in_circle/number_four_circle.svg" alt="number_four">
+                                            {{-- <i class="fa-solid fa-4 p-3"></i> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 mb-2">
+                            <div class="card">
+                                <div class="container">
+                                    <div class="row my-0 p-2">
+                                        <div class="col-sm-2 p-2">
+                                            <img class="" src="./assets/img/numbers_in_circle/number_five_circle.svg" alt="number_five">
+                                            {{-- <i class="fa-solid fa-5 fa-2xl my-3 p-3"></i> --}}
+                                        </div>
+                                        <div class="col-sm-2 text-start p-0">
                                             <h5 class="my-3">Gesamterlebnis mitteilen:</h5>
                                         </div>
-                                        <div class="col-sm-8 my-4">
+                                        <div class="col-sm-8">
                                             <div class="container">
-                                                <div class="row my-3">
-                                                    <div class="col">
+                                                <div class="row">
+                                                    <div class="col justify-content-evenly text-center p-2">
+                                                 
                                                         <!-- EXAMPLE POPOVER BS5 TOOLTIP START -->
-                                                        <i class="fa-solid fa-frown fa-2x" data-toggle="tooltip" data-placement="top" title="schlecht" data-value="1"></i>
+                                                        {{-- <i class="fa-solid fa-frown fa-2xl" data-toggle="tooltip" data-placement="top" title="schlecht" data-value="1"></i>
+                                                        <img class="img-fluid" src="./assets/img/smiley_red_mask.svg" alt="smiley-test"  data-toggle="tooltip" data-placement="top" title="angemessen" />
+                                                         --}}
                                                         <!-- EXAMPLE POPOVER BS5 TOOLTIP STOP -->
+                                                
+                                                        {{-- <i class="fa-solid fa-meh fa-2xl" data-value="2"></i> --}}
+                                                        <img class="img-fluid px-3" src="./assets/img/icons_var/smiley_green_successreal_great.svg" alt="smiley-great"  data-toggle="tooltip" data-placement="top" title="Alles OK!"  data-value="1"/>
+                                                        
+
+                                                        {{-- <img class="img-fluid" src="./assets/img/Subtractsmiley-test.svg" alt="smiley-test"  data-toggle="tooltip" data-placement="top" title="angemessen" /> --}}
+                                                        <img class="img-fluid px-3" src="./assets/img/icons_var/smiley_lightgreen_nice.svg" alt="smiley-nice"  data-toggle="tooltip" data-placement="top" title=""  data-value="2"/>
+                                                        
+
+                                                        {{-- <i class="fa-solid fa-smile fa-2xl" data-toggle="tooltip" data-placement="top" title="angemessen" data-value="3"></i> --}}
+                                                        <img class="img-fluid px-3" src="./assets/img/icons_var/smiley_yellow_okay.svg" alt="smiley-ok"  data-toggle="tooltip" data-placement="top" title="Angemessen"  data-value="3"/>
+                                                        
+
+                                                        {{-- <i class="fa-solid fa-grin fa-2xl" data-value="4"></i> --}}
+                                                        <img class="img-fluid px-3" src="./assets/img/icons_var/smiley_orange_meh.svg" alt="smiley-meh"  data-toggle="tooltip" data-placement="top" title=""  data-value="4"/>
+                                                        
+
+                                                        {{-- <i class="fa-solid fa-laugh-squint fa-2xl" data-toggle="tooltip" data-placement="top" title="super" data-value="5"></i> --}}
+                                                        <img class="img-fluid px-3" src="./assets/img/icons_var/smiley_red_mask.svg" alt="smiley-mask"  data-toggle="tooltip" data-placement="top" title="Stinkt ubelst - hazardous!!!"  data-value="5"/>
+                                                        
                                                     </div>
-                                                    <div class="col">
-                                                        <i class="fa-solid fa-meh fa-2xl" data-value="2"></i>
-                                                    </div>
-                                                    <div class="col">
-                                                        <i class="fa-solid fa-smile fa-2xl" data-toggle="tooltip" data-placement="top" title="angemessen" data-value="3"></i>
-                                                    </div>
-                                                    <div class="col">
-                                                        <i class="fa-solid fa-grin fa-2xl" data-value="4"></i>
-                                                    </div>
-                                                    <div class="col">
-                                                        <i class="fa-solid fa-laugh-squint fa-2xl" data-toggle="tooltip" data-placement="top" title="super" data-value="5" style="color:red;"></i>
-                                                    </div>
-                                                </div>
+                                                </div> 
                                             </div>
                                         </div>
                                     </div>
@@ -77,9 +117,9 @@
                         </div>
                         <div class="col-sm-12 mt-3">
                             <div class="row justify-content-center">
-                                <div class="d-flex justify-content-center">
-                                    <button class="btn btn-primary mx-sm-4" data-toggle="tooltip" data-placement="top" title="super">Feedback geben und beenden</button>
-                                    <button class="btn btn-primary ml-sm-4">Feedback geben und Tipps erhalten</button>
+                                <div class="d-flex justify-content-center mt-2 mb-4">
+                                    <button class="btn btn-lg btn-primary mx-sm-4" data-toggle="tooltip" data-placement="top" title="super">Feedback geben und beenden</button>
+                                    <button class="btn btn-lg btn-success ml-sm-4">Feedback geben und Tipps erhalten</button>
                                   </div> 
                             </div>
                         </div>
