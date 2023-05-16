c072f5e0cdd2ec4c8d3c7b10b08aeafdf86115b8
CyrSen <cyrill.senger@gmx.net>
2023-05-16 09:09:19 +0200

    alle tipps popups fertig

--- 2. frontend/script.js
+++ 2. frontend/script.js
@@ -1,46 +1,52 @@
-// Funktion zum Öffnen des Popups
-function openPopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'flex';
-  }
+
   
-  // Funktion zum Schließen des Popups
-  function closePopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'none';
-  }
-  
-  // Funktion zum Öffnen des Luft-Popups
-  function openPopupLuft() {
-    var popupLuft = document.getElementById('popupLuft');
-    popupLuft.style.display = 'flex';
-  }
-  
-  // Funktion zum Schließen des Luft-Popups
-  function closePopupLuft() {
-    var popupLuft = document.getElementById('popupLuft');
-    popupLuft.style.display = 'none';
-  }
-  
-  // Funktion zum Öffnen des Gesamtkomfort-Popups
-  function openPopupGesamtkomfort() {
-    var popupGesamtkomfort = document.getElementById('popupGesamtkomfort');
-    popupGesamtkomfort.style.display = 'flex';
-  }
-  
-  // Funktion zum Schließen des Gesamtkomfort-Popups
-  function closePopupGesamtkomfort() {
-    var popupGesamtkomfort = document.getElementById('popupGesamtkomfort');
-    popupGesamtkomfort.style.display = 'none';
-  }
-  
-  // Ereignislistener für den Öffnen-Button hinzufügen
-  document.getElementById('openPopup').addEventListener('click', openPopup);
-  document.getElementById('openPopupLuft').addEventListener('click', openPopupLuft);
-  document.getElementById('openPopupGesamtkomfort').addEventListener('click', openPopupGesamtkomfort);
-  
-  // Ereignislistener für den Schließen-Button hinzufügen
-  document.getElementById('closePopup').addEventListener('click', closePopup);
-  document.getElementById('closePopupLuft').addEventListener('click', closePopupLuft);
-  document.getElementById('closePopupGesamtkomfort').addEventListener('click', closePopupGesamtkomfort);
-  
+  // Funktion zum Öffnen des Zu-kalt-Popups
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'flex';
+}
+
+// Funktion zum Schließen des Zu-kalt-Popups
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'none';
+}
+
+// Funktion zum Öffnen des Zu-heiss-Popups
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'flex';
+}
+
+// Funktion zum Schließen des Zu-heiss-Popups
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'none';
+}
+
+// Funktion zum Öffnen des Bürolärm-Popups
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'flex';
+}
+
+// Funktion zum Schließen des Bürolärm-Popups
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'none';
+}
+
+
+// Funktion zum Öffnen des Schlechte-Luft-Popups
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'flex';
+}
+
+// Funktion zum Schließen des Schlechte-Luft-Popups
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = 'none';
+}
+
+
