55775d072fb30e90236543efa048cb08b75faf9d
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-05-25 14:37:07 +0200

    final css setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,178 +26,178 @@
 
   
     <style>
-html {
-  scroll-behavior: smooth;
-}
+    html {
+    scroll-behavior: smooth;
+    }
 
-#intro {
-  background: url("img/panorama-3094696_1920.jpg") no-repeat center center;
-  background-size: cover;
-}
+    #intro {
+    background: url("img/panorama-3094696_1920.jpg") no-repeat center center;
+    background-size: cover;
+    }
 
-.top-nav-collapse {
-  background-color: #3949ab !important;
-}
+    .top-nav-collapse {
+    background-color: #3949ab !important;
+    }
 
-.navbar:not(.top-nav-collapse) {
-  background: transparent !important;
-}
+    .navbar:not(.top-nav-collapse) {
+    background: transparent !important;
+    }
 
-@media (max-width: 768px) {
-  .navbar:not(.top-nav-collapse) {
-    background: #3949ab !important;
-  }
-}
+    @media (max-width: 768px) {
+    .navbar:not(.top-nav-collapse) {
+        background: #3949ab !important;
+    }
+    }
 
-#intro .h6 {
-  font-weight: 300;
-  line-height: 1.7;
-}
+    #intro .h6 {
+    font-weight: 300;
+    line-height: 1.7;
+    }
 
-.site-bg-img {
-  height: 100vh;
-  min-height: 480px;
-  max-height: 1080px;
-}
+    .site-bg-img {
+    height: 100vh;
+    min-height: 480px;
+    max-height: 1080px;
+    }
 
-.hm-gradient .site-bg-img {
-  background: rgba(42, 27, 161, 0.7);
-  background: linear-gradient(45deg, rgba(42, 27, 161, 0.7), rgba(29, 210, 177, 0.7) 100%);
-}
+    .hm-gradient .site-bg-img {
+    background: rgba(42, 27, 161, 0.7);
+    background: linear-gradient(45deg, rgba(42, 27, 161, 0.7), rgba(29, 210, 177, 0.7) 100%);
+    }
 
-#feedback {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background: url("img/architecture-4000.jpg") no-repeat center center;
-  background-size: cover;
-  position: relative;
-  z-index: 1;
-}
+    #feedback {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background: url("img/architecture-4000.jpg") no-repeat center center;
+    background-size: cover;
+    position: relative;
+    z-index: 1;
+    }
 
-.rgba-black-strong {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Deckkraft hier ändern */
-  z-index: -1;
-}
+    .rgba-black-strong {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Deckkraft hier ändern */
+    z-index: -1;
+    }
 
-.card.mb-5.wow.fadeInLeft[data-wow-delay=".4s"],
-.card.mb-5.wow.fadeInRight[data-wow-delay=".4s"] {
-  flex: 1;
-}
+    .card.mb-5.wow.fadeInLeft[data-wow-delay=".4s"],
+    .card.mb-5.wow.fadeInRight[data-wow-delay=".4s"] {
+    flex: 1;
+    }
 
-.card-body.p-5 {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
+    .card-body.p-5 {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    }
 
-@media (max-width: 450px) {
-  .margins {
-    margin-right: 1rem;
-    margin-left: 1rem;
-  }
-}
+    @media (max-width: 450px) {
+    .margins {
+        margin-right: 1rem;
+        margin-left: 1rem;
+    }
+    }
 
-footer {
-  position: sticky;
-  bottom: 0;
-}
+    footer {
+    position: sticky;
+    bottom: 0;
+    }
 
-#login {
-  display: flex;
-  align-items: stretch;
-  min-height: 100vh;
-  background: url("img/skyscraper-with-clouds-reflection.jpg") no-repeat center center;
-  background-size: cover;
-  position: relative;
-  z-index: 1;
-}
+    #login {
+    display: flex;
+    align-items: stretch;
+    min-height: 100vh;
+    background: url("img/skyscraper-with-clouds-reflection.jpg") no-repeat center center;
+    background-size: cover;
+    position: relative;
+    z-index: 1;
+    }
 
-.rgba-black-strong {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.7); /* Deckkraft hier ändern */
-  z-index: -1;
-}
+    .rgba-black-strong {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7); /* Deckkraft hier ändern */
+    z-index: -1;
+    }
 
-.card.mb-5.wow.fadeInUp[data-wow-delay=".4s"] {
-  flex: 1;
-}
+    .card.mb-5.wow.fadeInUp[data-wow-delay=".4s"] {
+    flex: 1;
+    }
 
-.card-body.p-5 {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
+    .card-body.p-5 {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    }
 
-#info {
-  display: flex;
-  align-items: stretch;
-  min-height: 100vh;
-  background: url("img/open-window.jpg") no-repeat center center;
-  background-size: cover;
-  position: relative;
-  z-index: 1;
-}
+    #info {
+    display: flex;
+    align-items: stretch;
+    min-height: 100vh;
+    background: url("img/open-window.jpg") no-repeat center center;
+    background-size: cover;
+    position: relative;
+    z-index: 1;
+    }
 
-.rgba-black-strong {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.7); /* Deckkraft hier ändern */
-  z-index: -1;
-}
+    .rgba-black-strong {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7); /* Deckkraft hier ändern */
+    z-index: -1;
+    }
 
-.card.mb-5.wow.fadeInUp[data-wow-delay=".4s"] {
-  flex: 1;
-}
+    .card.mb-5.wow.fadeInUp[data-wow-delay=".4s"] {
+    flex: 1;
+    }
 
-.card-body.p-5 {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
+    .card-body.p-5 {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    }
 
-#popup .modal-dialog-centered {
-  margin: 0 auto;
-}
+    #popup .modal-dialog-centered {
+    margin: 0 auto;
+    }
 
-.modal-header.bg-indigo {
-  background-color: #3949ab;
-  color: #ffffff;
-}
+    .modal-header.bg-indigo {
+    background-color: #3949ab;
+    color: #ffffff;
+    }
 
-.modal-body {
-  padding: 1.5rem;
-}
+    .modal-body {
+    padding: 1.5rem;
+    }
 
-.btn-secondary {
-  background-color: #3949ab;
-  color: #ffffff;
-}
+    .btn-secondary {
+    background-color: #3949ab;
+    color: #ffffff;
+    }
 
-.modal.shadow {
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
+    .modal.shadow {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
 
-.modal-dialog.modal-dialog-centered {
-  max-width: 1000px; /* Anpassen der maximalen Breite */
-  margin: 0 auto; /* Zentrierte Ausrichtung */
-}
+    .modal-dialog.modal-dialog-centered {
+    max-width: 1000px; /* Anpassen der maximalen Breite */
+    margin: 0 auto; /* Zentrierte Ausrichtung */
+    }
 
 
     </style>
