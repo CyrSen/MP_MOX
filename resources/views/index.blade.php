dcf93a22752b0de2edd2c9e207fe64ce31ba12ed
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-04 15:26:30 +0200

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,26 +55,26 @@
                             </div> --}}
                         @endauth
 
-                        <div class=" mt-3">
-                            {{--  <ul class="navbar-nav me-auto">
-                  @if (Route::has('login'))
-                  @auth
-                  <li class="nav-item">
-                      <a href="{{ url('/  ') }}" class="nav-link waves-effect waves-light">Home</a>
-                  </li>        
-                  @else
-                  <li class="nav-item">
-                      <a href="{{ route('login') }}" class="nav-item">Log in</a>
-                  </li>
-                  @if (Route::has('register'))
-                  <li class="nav-item">
-                      <a href="{{ route('register') }}" class="nav-item">Register</a>
-                  </li>
-                  @endif
-                  @endauth
-                  @endif
-                </ul> --}}
-                        </div>
+                        {{--  <div class=" mt-3">
+                            <ul class="navbar-nav me-auto">
+                                @if (Route::has('login'))
+                                @auth
+                                <li class="nav-item">
+                                    <a href="{{ url('/  ') }}" class="nav-link waves-effect waves-light">Home</a>
+                                </li>        
+                                @else
+                                <li class="nav-item">
+                                    <a href="{{ route('login') }}" class="nav-item">Log in</a>
+                                </li>
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a href="{{ route('register') }}" class="nav-item">Register</a>
+                                </li>
+                                @endif
+                                @endauth
+                                @endif
+                                </ul>
+                        </div> --}}
                     </div>
                     <div class="arrow-circle mb-5">
                         <a class="arrow-link" href="#nutzen">
