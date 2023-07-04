5ab698378d86ff253865a059df465a6ae076839e
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-04 15:55:38 +0200

    try

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -66,7 +66,7 @@
             min-height: 100vh;
             position: relative;
             min-width: 100vw;
-            background-image: url("./assets/img/panorama-3094696_1920.jpg") no-repeat center center;
+            background-image: url("./assets/img/panorama-3094696_1920.jpg");
             background-size: cover;
             z-index: 1;
             padding-top: 10%;
@@ -80,9 +80,10 @@
                 background-size: 100% 100%;
                 position: relative;
                 min-width: 100vw;
-                background-image: url("./assets/img/panorama-3094696_1920.jpg") no-repeat center center;
+                background-image: url("./assets/img/panorama-3094696_1920.jpg");
                 z-index: 1;
                 padding-top: 10%;
+                background-size: cover;
             }
         }
 
@@ -94,7 +95,7 @@
                 background-size: 100% 100%;
                 position: relative;
                 min-width: 100vw;
-                background-image: url("./assets/img/panorama-3094696_1920.jpg") no-repeat center center;
+                background-image: url("./assets/img/panorama-3094696_1920.jpg");
                 z-index: 1;
                 padding-top: 10%;
             }
