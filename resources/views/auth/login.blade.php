cdc1453c9cf8c3925ffd03a558bee283da203612
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-09 15:22:18 +0200

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <section id="login">
     <div class="rgba-black-strong"></div>
         <div class="container py-5">
-            <div class="animate__animated animate__fadeInDown animate__delay-1s">
+            <div id="animationDown" class="">
                 <h2 class="h1 text-white pt-5 pb-3 text-center">Login</h2>
                 <p class="text-white px-5 mb-4 pb-3 lead text-center">
                 Bitte loge dich ein, um myOffice<span class="">X</span> zu nutzen.
@@ -13,7 +13,7 @@
             </div>
             <div class="row justify-content-center">
                 <div class="col-md-6">
-                <div class="card mb-5 animate__animated animate__fadeInUp animate__delay-1s" style="max-width: 800px;">
+                <div id="animationUp" class="card mb-5" style="max-width: 800px;">
                     <div class="card-body p-3 pb-5 pt-5">
                 <!-- Form start-->
                 <div class="row justify-content-center">
@@ -69,3 +69,18 @@
   </section>
 
   @endsection 
+
+  <script>
+      window.addEventListener('load', function() {
+    var element = document.getElementById('animationUp');
+    element.classList.add('animate__animated', 'animate__fadeInUp');
+  });
+
+  window.addEventListener('load', function() {
+    var element = document.getElementById('animationDown');
+    element.classList.add('animate__animated', 'animate__fadeInDown');
+  });
+
+
+  
+  </script>
