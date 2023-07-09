cd38817f6e5ddad864a215e909934faa1755591f
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-09 21:50:57 +0200

    undo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,18 +63,6 @@
         background-color: #3f51b5 !important;
         }
 
-        @media screen and (max-width: 600px) {
-   table {width:100%;}
-   thead {display: none;}
-   tr:nth-of-type(2n) {background-color: inherit;}
-   tr td:first-child {background: #f0f0f0; font-weight:bold;font-size:1.3em;}
-   tbody td {display: block;  text-align:center;}
-   tbody td:before { 
-       content: attr(data-th); 
-       display: block;
-       text-align:center;  
-   }
-}
 
         
 
