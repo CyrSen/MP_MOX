de12c72924b1393d37927e9d0d3b3068d25d525f
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-10 18:53:53 +0200

    back

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <section id="login">
     <div class="rgba-black-strong"></div>
         <div class="container py-5">
-            <div id="{{-- animationDown --}}" class="wow fadeInLeft" data-wow-delay=".3s">
+            <div id="{{-- animationDown --}}" class=" animate__animated animate__fadeInDown animate__delay-1s">
                 <h2 class="h1 text-white mt-2 mt-md-0 pt-3 pt-md-5 pb-3 text-center">Login</h2>
                 <p class="text-white px-3 px-md-5 mb-4 pb-1pb-md-3 lead text-center">
                 Bitte loge dich ein, um myOffice<span class="">X</span> zu nutzen.
@@ -13,7 +13,7 @@
             </div>
             <div class="row justify-content-center">
                 <div class="col-md-6">
-                <div id="{{-- animationUp --}}" class="card mb-5 wow fadeInRight" data-wow-delay=".3s" style="max-width: 800px;">
+                <div id="{{-- animationUp --}}" class="card mb-5 animate__animated animate__fadeInUp animate__delay-1s" style="max-width: 800px;">
                     <div class="card-body p-3 pb-5 pt-5">
                 <!-- Form start-->
                 <div class="row justify-content-center">
