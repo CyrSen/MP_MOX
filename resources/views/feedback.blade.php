dcb39f75054387bc6c823fa6405db575acffe5d1
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-11 16:01:04 +0200

    test

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -13,9 +13,11 @@
             <form action="{{ route('feedback.store', ['feedbackMapId' => $feedbackMapId]) }}" method="POST">
                 @csrf
                 <div class="row justify-content-center mt-1 mt-md-3 pt-4 pt-md-0">
-                    <div id="{{-- animationLeft --}}" class="feedbackPanels col-md-6 mt-3 mb-1 mb-md-5 animate__animated animate__fadeInLeft animate__delay-1s">
+                    <div id="{{-- animationLeft --}}"
+                        class="feedbackPanels col-md-6 mt-3 mb-1 mb-md-5 animate__animated animate__fadeInLeft animate__delay-1s">
                         <div class="card mx-auto h-100" style="background-color: rgba(233, 228, 224, 0.8);">
-                            <div class="card-body d-flex flex-column justify-content-center align-items-center pt-1 pb-0 px-1 px-md-3">
+                            <div
+                                class="card-body d-flex flex-column justify-content-center align-items-center pt-1 pb-0 px-1 px-md-3">
                                 <!-- Content of Card 1 -->
                                 <div class="col">
                                     <h2 class="h1 mb-3 px-3 pt-3">Feedback geben</h2>
@@ -30,18 +32,19 @@
                                     <input id="x-coordinates" type="hidden" name="x_coordinates">
                                     <input id="y-coordinates" type="hidden" name="y_coordinates">
                                 </div>
-            </form>
+            {{-- </form> --}}
             <!-- FORM END-->
 
             <!--ADMIN USE -->
-            <div class="col-sm-3" style="display:none;">
-                <p id="x-display"></p>
-                <p id="y-display"></p>
+                <div class="col-sm-3" style="display:none;">
+                        <p id="x-display"></p>
+                        <p id="y-display"></p>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
-        </div>
-        <div id="{{-- animationRight --}}" class="feedbackPanels col-md-6 mt-3 mb-5 animate__animated animate__fadeInRight animate__delay-1s">
+        <div id="{{-- animationRight --}}"
+            class="feedbackPanels col-md-6 mt-3 mb-5 animate__animated animate__fadeInRight animate__delay-1s">
             <div class="card mx-auto h-100" style="background-color: rgba(233, 228, 224, 0.8);">
                 <div class="card-body px-3 px-md-5 py-3">
                     <div class="form-group mt-3">
@@ -97,15 +100,15 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_notloud.svg" alt="smiley-nice"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Leise" data-bs-value="3"
-                                    onclick="setNoiseLevel(3)" style="position: relative; left: 3px;">
+                                    data-bs-placement="top" title="Leise" data-bs-value="3" onclick="setNoiseLevel(3)"
+                                    style="position: relative; left: 3px;">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_excellent.svg" alt="smiley-ok"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Optimal" data-bs-value="5"
-                                    onclick="setNoiseLevel(5)" style="position: relative; left: 8px;">
+                                    data-bs-placement="top" title="Optimal" data-bs-value="5" onclick="setNoiseLevel(5)"
+                                    style="position: relative; left: 8px;">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
@@ -118,8 +121,8 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_loud_red.svg" alt="smiley-mask"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Zu laut" data-bs-value="1"
-                                    onclick="setNoiseLevel(1)" style="position: relative;left: 10px;">
+                                    data-bs-placement="top" title="Zu laut" data-bs-value="1" onclick="setNoiseLevel(1)"
+                                    style="position: relative;left: 10px;">
                             </div>
                         </div>
                         <input type="hidden" name="noise_level" id="noise-level-input">
@@ -140,8 +143,7 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_good.svg" alt="smiley-nice"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Gut" data-bs-value="4"
-                                    onclick="setAirQLevel(4)">
+                                    data-bs-placement="top" title="Gut" data-bs-value="4" onclick="setAirQLevel(4)">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
@@ -154,15 +156,14 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_poor.svg" alt="smiley-meh"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Mässig" data-bs-value="2"
-                                    onclick="setAirQLevel(2)">
+                                    data-bs-placement="top" title="Mässig" data-bs-value="2" onclick="setAirQLevel(2)">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_smelly.svg" alt="smiley-mask"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Schlecht"
-                                    data-bs-value="1" onclick="setAirQLevel(1)">
+                                    data-bs-placement="top" title="Schlecht" data-bs-value="1"
+                                    onclick="setAirQLevel(1)">
                             </div>
                         </div>
                         <input type="hidden" name="air_quality_level" id="air-quality-level-input">
@@ -183,8 +184,7 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_good.svg" alt="smiley-nice"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Gut" data-bs-value="4"
-                                    onclick="setHiggeLevel(4)">
+                                    data-bs-placement="top" title="Gut" data-bs-value="4" onclick="setHiggeLevel(4)">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
@@ -197,8 +197,7 @@
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
                                     src="./assets/img/icons_var/smiley_new_poor.svg" alt="smiley-meh"
                                     data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip"
-                                    data-bs-placement="top" title="Mässig" data-bs-value="2"
-                                    onclick="setHiggeLevel(2)">
+                                    data-bs-placement="top" title="Mässig" data-bs-value="2" onclick="setHiggeLevel(2)">
                             </div>
                             <div class="icon-wrapper icon-element">
                                 <img class="feedbackEmojis img-fluid  p-2 tooltip-icon"
@@ -236,14 +235,13 @@
 @endsection
 
 <script>
-   document.addEventListener('DOMContentLoaded', function() {
-    var element = document.getElementById('animationLeft');
-    element.classList.add('animate__animated', 'animate__fadeInLeft');
-  });
+    document.addEventListener('DOMContentLoaded', function() {
+        var element = document.getElementById('animationLeft');
+        element.classList.add('animate__animated', 'animate__fadeInLeft');
+    });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var element = document.getElementById('animationRight');
-    element.classList.add('animate__animated', 'animate__fadeInRight');
-  });
+    document.addEventListener('DOMContentLoaded', function() {
+        var element = document.getElementById('animationRight');
+        element.classList.add('animate__animated', 'animate__fadeInRight');
+    });
 </script>
-
