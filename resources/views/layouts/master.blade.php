d201978b789d83e92bfd3e436c7667aff60bd795
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-11 19:56:10 +0200

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -267,8 +267,8 @@
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script> --}}
     <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/popper.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/wow.min.js') }}"></script>
