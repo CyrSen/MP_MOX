7c28d75788f5d61f618dbf8f9946f20057b9e087
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-12 13:26:37 +0200

    done

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -215,55 +215,32 @@
                 var airQualityLevel = row.querySelector('td:nth-child(6)').textContent;
                 var higgeLevel = row.querySelector('td:nth-child(7)').textContent;
 
-                var createIcon = function(level, x, y, category) {
-    var icon = document.createElement('i');
-    icon.classList.add('fa', 'fa-solid');
+                // Create an icon element
+                var createIcon = function(level, x, y) {
+                    var icon = document.createElement('i');
+                    icon.classList.add('fa', 'fa-solid');
 
-    // Set the emoji based on the level and category
-    if (category === 'temperature') {
-        switch (level) {
-            case '1':
-                icon.classList.add('fa-lowtemp');
-                break;
-            case '2':
-                icon.classList.add('fa-frown-open', 'frownopen');
-                break;
-            case '3': 
-                icon.classList.add('fa-meh', 'mehie');
-                break;
-            case '4':
-                icon.classList.add('fa-face-laugh', 'laugh');
-                break;
-            case '5':
-                icon.classList.add('fa-hightemp');
-                break;
-            default:
-                icon.classList.add('fa-comment-medical', 'comment-medical');
-                break;
-        }
-    } else {
-        // For other categories, use the existing logic
-        switch (level) {
-            case '1':
-                icon.classList.add('fa-frown', 'frownie');
-                break;
-            case '2':
-                icon.classList.add('fa-frown-open', 'frownopen');
-                break;
-            case '3':
-                icon.classList.add('fa-meh', 'mehie');
-                break;
-            case '4':
-                icon.classList.add('fa-face-laugh', 'laugh');
-                break;
-            case '5':
-                icon.classList.add('fa-laugh-squint', 'squint');
-                break;
-            default:
-                icon.classList.add('fa-comment-medical', 'comment-medical');
-                break;
-        }
-    }
+                    // Set the emoji based on the level
+                    switch (level) {
+                        case '1':
+                            icon.classList.add('fa-frown', 'frownie');
+                            break;
+                        case '2':
+                            icon.classList.add('fa-frown-open', 'frownopen');
+                            break;
+                        case '3':
+                            icon.classList.add('fa-meh', 'mehie');
+                            break;
+                        case '4':
+                            icon.classList.add('fa-face-laugh', 'laugh');
+                            break;
+                        case '5':
+                            icon.classList.add('fa-laugh-squint', 'squint');
+                            break;
+                        default:
+                            icon.classList.add('fa-comment-medical', 'comment-medical');
+                            break;
+                    }
 
                     icon.classList.add('adminView');
                     icon.classList.add('tooltip-icon');
@@ -280,8 +257,7 @@
 
                 // Add the icons after the image containers
                 mapNoiseContainer.appendChild(createIcon(noiseLevel, xCoordinates, yCoordinates));
-                mapTemperatureContainer.appendChild(createIcon(temperatureLevel, xCoordinates, yCoordinates, 'temperature'));
-
+                mapTemperatureContainer.appendChild(createIcon(temperatureLevel, xCoordinates, yCoordinates));
                 mapAirQualityContainer.appendChild(createIcon(airQualityLevel, xCoordinates, yCoordinates));
                 mapHiggeContainer.appendChild(createIcon(higgeLevel, xCoordinates, yCoordinates));
             } else {
