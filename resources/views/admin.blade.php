bb0ee77d1a24aad09334b6cfac67f235cce56d3f
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-12 15:02:46 +0200

    test

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -227,27 +227,24 @@
         switch (level) {
             case '1':
                 icon.src = './assets/img/icons_var/smiley_new_cold.svg';
-                icon.class = 'imageIcons';
+                icon.style.width = '1.5rem';
                 icon.alt = 'Temperature Low';
                 break;
             case '2':
                 icon.src = 'path-to-meh-image';
-                icon.class = 'imageIcons';
                 icon.alt = 'Meh';
                 break;
             case '3':
                 icon.src = 'path-to-laugh-image';
-                icon.class = 'imageIcons';
                 icon.alt = 'Laugh';
                 break;
             case '4':
                 icon.src = 'path-to-meh-image';
-                icon.class = 'imageIcons';
                 icon.alt = 'Meh';
                 break;
             case '5':
                 icon.src = './assets/img/icons_var/smiley_new_hot_red.svg';
-                icon.class = 'imageIcons';
+                icon.style.width = '1.5rem';
                 icon.alt = 'Temperature High';
                 break;
             default:
