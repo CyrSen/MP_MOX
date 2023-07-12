f186d0beb8cce101ff204e41dc9cf5adb8cb24f9
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-12 14:02:04 +0200

    ss

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -280,7 +280,7 @@
 
                 // Add the icons after the image containers
                 mapNoiseContainer.appendChild(createIcon(noiseLevel, xCoordinates, yCoordinates));
-                mapTemperatureContainer.appendChild(createIcon(temperatureLevel, xCoordinates, yCoordinates));
+                mapTemperatureContainer.appendChild(createIcon(temperatureLevel, xCoordinates, yCoordinates, 'temperature'));
                 mapAirQualityContainer.appendChild(createIcon(airQualityLevel, xCoordinates, yCoordinates));
                 mapHiggeContainer.appendChild(createIcon(higgeLevel, xCoordinates, yCoordinates));
             } else {
