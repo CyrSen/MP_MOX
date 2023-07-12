6c49e3ed3504bdc2b5b0b543e3a73a247fac0971
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-12 12:30:27 +0200

    test

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -260,11 +260,14 @@
     return icon;
 };
 
+
                 // Add the icons after the image containers
                 mapNoiseContainer.appendChild(createIcon(noiseLevel, xCoordinates, yCoordinates));
                 mapTemperatureContainer.appendChild(createIcon(temperatureLevel, xCoordinates, yCoordinates));
                 mapAirQualityContainer.appendChild(createIcon(airQualityLevel, xCoordinates, yCoordinates));
                 mapHiggeContainer.appendChild(createIcon(higgeLevel, xCoordinates, yCoordinates));
+                mapTemperatureContainer.appendChild(createIcon(temperatureIconClass, xCoordinates, yCoordinates));
+
             } else {
                 row.style.display = 'none';
             }
