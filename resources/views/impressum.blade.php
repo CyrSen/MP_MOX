dc02fe2196fdca66e766b96d9932e915f34afb8b
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-11-06 16:12:17 +0100

    d-none

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -97,7 +97,7 @@
                 </p>
 
                 <h4 class="mt-5 mb-3"><strong>Entwickler</strong></h4>
-                <p class="blue-grey-text custom-font-weight d-none">
+                <p class="blue-grey-text custom-font-weight" style="display:none;">
                     <h5 class=""><b><i>Cyrill Senger:</i></b></h5>
                     Projektmanagment, Konzeption, UX-Research und Frontend-Design
                     <br><br>
